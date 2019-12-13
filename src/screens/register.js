fb50c5c6a5b20c2f04ff6ab049b3b301426e2acc
saikiranjalagari <48744125+saikiranjalagari@users.noreply.github.com>
2019-12-13 15:07:04 +0530

    Update register.js

--- src/screens/register.js
+++ src/screens/register.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { StyleSheet, ScrollView, TextInput, Text, TouchableOpacity, Alert } from 'react-native';
 import { TextInputLayout } from 'rn-textinputlayout';
-// import styles from '../styles/login.css'
 import axios from 'axios'
 import Logo from "./logo";
  
